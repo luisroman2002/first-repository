3a096e3ac01a2715132c3778638a55ee6e554d66
luisroman2002 <luisroman2002@gmail.com>
2022-02-09 18:40:35 -0400

    Add operators files

---
+++ operadores.c
@@ -0,0 +1,26 @@
+#include  <stdio.h>
+
+    int main() {
+    
+    int notas = 15;
+	    if ("notas = 15 R: %d\n",(notas >= 0 && notas <= 9)){
+           printf(" usted no posee las notas necesarias para aprovar el curso \n");
+   	  
+           } else { 
+    	    printf("usted posee las notas necesarias para aprobar el curso \n");
+	   }
+    int notas2 = 9;
+            if ("notas2 = 9  R: %d\n",(notas2 >= 0 && notas2 <= 9)){
+           printf("usted no posee las notas necesarias para aprovar el curso \n");
+
+           } else {
+            printf("usted posee las notas necesarias para aprobar el curso \n");
+           }
+
+    
+    
+    
+    
+    
+    return 0;
+    } 
